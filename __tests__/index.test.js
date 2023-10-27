9ad9652f6f9fe9176f3704eb5435593375d216fd
Даниил <daniil@MacBook-Pro-konstantin-2.local>
2023-10-27 23:52:38 +0300

    edit validateString

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -20,3 +20,33 @@ test('checkValidStringRequired', () => {
   expect(checkRequired.isValid('str')).toBeTruthy();
   expect(checkRequired.isValid('is valid string')).toBeTruthy();
 });
+
+test('checkValidStringContains', () => {
+  const string = schema.string();
+  const expected1 = string.contains('World').isValid('Hello, World!'); 
+  const expected2 = string.contains('Hello').isValid('Hello, World!');
+  const expected3 = string.contains('').isValid('Hello, World!');
+  const expected4 = string.contains().isValid('Hello, World!');
+
+  expect(expected1).toBeTruthy();
+  expect(expected2).toBeTruthy();
+  expect(expected3).toBeTruthy();
+  expect(expected4).toBeTruthy();
+
+  const containsFaild = string.contains('Hello, World!');
+
+  expect(containsFaild.isValid('Fail')).toBeFalsy();
+  expect(containsFaild.isValid('Hello')).toBeFalsy();
+})
+
+test('checkMinLength', () => {
+  const check = schema.string().minLength(5);
+
+  expect(check.isValid('fail')).toBeFalsy();
+  expect(check.isValid('')).toBeFalsy();
+  expect(check.isValid()).toBeFalsy();
+
+  const defaultLength = schema.string().minLength();
+
+  expect(defaultLength.isValid('TestSucces')).toBeTruthy();
+})
--- src/utilit/string.js
+++ src/utilit/string.js
@@ -1,14 +1,35 @@
+const checkContains = (checkStr = '') => (str) => {
+  const checkValidStr = checkStr;
+  return str.includes(checkValidStr);
+}
+
+const checkMinLength = (lengthStr = 0) => (str = '') => {
+  if (str === '') return false;
+  const minLength = lengthStr;
+  return str.length >= minLength;
+}
+
 const params = {
   main: () => true,
   required: (str) => (!!(typeof str === 'string' && str !== '')),
+  contains: checkContains,
+  minLength: checkMinLength,
 };
 
 export default class String {
-  constructor(isValidParams = params.main) {
-    this.isValid = isValidParams;
+  constructor(ValidParams = params.main) {
+    this.isValid = ValidParams;
   }
 
   required() {
     return new String(params.required);
+  }
+
+  contains(str) {
+    return new String(params.contains(str));
+  }
+
+  minLength(lengthStr) {
+    return new String(params.minLength(lengthStr));
   }
 }
