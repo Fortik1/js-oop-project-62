39515b51326cded42b8cdb453d84ae929546a9fb
Даниил <daniil@MacBook-Pro-konstantin-2.local>
2023-10-29 14:25:24 +0300

    check test

--- src/utilit/string.js
+++ src/utilit/string.js
@@ -13,7 +13,7 @@ const checkMinLength = (lengthStr = 0) => (str = '') => {
 };
 
 const params = {
-  main: (str) => str === 'undefiend' ? false : true,
+  main: (str) => str === '' ? true : !!str,
   required: (str) => (typeof str === 'string' && str !== '' && str !== null ),
   contains: checkContains,
   minLength: checkMinLength,
