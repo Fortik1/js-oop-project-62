2849ce708b02d2df4e390513d80167b908bee72a
Даниил <daniil@MacBook-Pro-konstantin-2.local>
2023-10-29 17:53:37 +0300

    fix object

--- src/utilit/object.js
+++ src/utilit/object.js
@@ -1,18 +1,18 @@
 export default class CheckObject {
   constructor(fn) {
+    this.isValid = () => true;
     this.fn = fn;
   }
 
-  isValid(obj) {
-    if (obj === null || typeof obj !== 'object') return false;
-    
-    for (const [name, value] of Object.entries(obj)) {
-      if (!this.paramsCheck[name].isValid(value)) return false;
-    }
-    return true;
-  }
-
   shape(paramsCheck) {
+    this.isValid = (obj) => {
+      if (obj === null || typeof obj !== 'object') return false;
+        
+        for (const [name, value] of Object.entries(obj)) {
+          if (!this.paramsCheck[name].isValid(value)) return false;
+        }
+        return true;
+    };
     this.paramsCheck = paramsCheck;
   }
 
